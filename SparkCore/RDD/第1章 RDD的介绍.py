8d5189f567e2f5def0c245a86e2fbefa00e850ae
wux_labs@outlook.com <wux_labs@outlook.com>
2022-04-18 10:28:19 +0000

    RDD介绍

--- SparkCore/RDD/第1章 RDD的介绍.py
+++ SparkCore/RDD/第1章 RDD的介绍.py
@@ -25,28 +25,67 @@
 # MAGIC * 数据计算API
 # MAGIC * 等等
 # MAGIC 
-# MAGIC 我们需要一个统一的数据抽象来实现上述分布式计算所需的功能，这个数据抽象就是RDD。
+# MAGIC 这些功能，不能简单的通过Python内置的本地集合对象(如 List)去完成。
+# MAGIC 
+# MAGIC 我们在分布式框架中，我们需要一个统一的数据抽象来实现上述分布式计算所需的功能，这个数据抽象就是RDD。
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC ## 1.2 什么是RDD
 # MAGIC RDD(Resilient Distributed Dataset)叫做弹性分布式数据集，是Spark中最基本的数据抽象，代表一个不可变、可分区、元素可并行计算的集合。
+# MAGIC 
+# MAGIC * Dataset：一个数据集合，用于存放数据的。
+# MAGIC * Distributed：RDD中的数据是分布式存储的，可用于分布式计算。
+# MAGIC * Resilient：RDD中的数据可以存储在内存中或者磁盘中。
+# MAGIC 
+# MAGIC RDD
+# MAGIC * 不可变：immutable
+# MAGIC   * 不可变集合
+# MAGIC   * 变量的声明使用val
+# MAGIC * 可分区：partitioned
+# MAGIC   * 集合的数据被划分为很多部分
+# MAGIC   * 每部分称为分区 partition
+# MAGIC * 并行计算：parallel
+# MAGIC   * 集合中的数据可以被并行的计算处理
+# MAGIC   * 每个分区数据被一个Task任务处理
+# MAGIC 
+# MAGIC 所有的运算以及操作都建立在RDD数据结构的基础之上。
+# MAGIC 
+# MAGIC 可以认为RDD是分布式的列表List或数组Array，抽象的数据结构，RDD是一个抽象类Abstract Class和泛型Generic Type。
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC abstract class RDD[T: ClassTag](
+# MAGIC     @transient private var _sc: SparkContext,
+# MAGIC     @transient private var deps: Seq[Dependency[_]]
+# MAGIC   ) extends Serializable with Logging {
+# MAGIC   // TODO ...
+# MAGIC }
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC ## 1.3 RDD的五大特性
+# MAGIC 
+# MAGIC RDD 数据结构内部有五个特性（摘录RDD 源码）：前三个特征每个RDD都具备的，后两个特征可选的。
+# MAGIC 
+# MAGIC Internally, each RDD is characterized by five main properties:
+# MAGIC 
 # MAGIC * A list of partitions.(RDD是有分区的)
 # MAGIC * A function for computing each split.(计算方法都会作用到每个分区上)
 # MAGIC * A list of dependencies on other RDDs.(RDD之间是有依赖关系的)
-# MAGIC * Optionally, a Partitioner for key-value RDDs.(KV型的RDD可以有分区器)
-# MAGIC * Optionally, a list of preferred locations to compute each split on.(RDD分区数据的读取会尽量靠近数据所在地)
+# MAGIC * Optionally, a Partitioner for key-value RDDs (e.g. to say that the RDD is hash-partitioned).(KV型的RDD可以有分区器)
+# MAGIC * Optionally, a list of preferred locations to compute each split on (e.g. block locations for an HDFS file).(RDD分区数据的读取会尽量靠近数据所在地)
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC ### 特性1 RDD是有分区的
+# MAGIC 
+# MAGIC - A list of partitions
+# MAGIC 
 # MAGIC 分区是RDD数据存储的最小单位。  
 # MAGIC 一份RDD的数据，本质上是分割成了多个分区。
 
@@ -60,6 +99,9 @@ print("RDD的分区情况：",rdd.glom().collect())
 
 # MAGIC %md
 # MAGIC ### 特性2 计算方法都会作用到每个分区上
+# MAGIC 
+# MAGIC - A function for computing each split
+# MAGIC 
 # MAGIC 以下代码中的 x * 10 会作用到每个分区的每个元素上。
 
 # COMMAND ----------
@@ -71,6 +113,9 @@ rdd.glom().collect()
 
 # MAGIC %md
 # MAGIC ### 特性3 RDD之间是有依赖关系的
+# MAGIC 
+# MAGIC - A list of dependencies on other RDDs
+# MAGIC 
 # MAGIC 如下代码之间是有依赖关系的  
 # MAGIC `textFile`->`rdd1`->`rdd2`->`rdd3`->`rdd4`
 
@@ -86,6 +131,9 @@ rdd4.collect()
 
 # MAGIC %md
 # MAGIC ### 特性4 Key-Value型的RDD可以有分区器
+# MAGIC 
+# MAGIC - Optionally, a Partitioner for key-value RDDs (e.g. to say that the RDD is hash-partitioned)
+# MAGIC 
 # MAGIC Key-Value RDD：RDD中存储的是二元组，比如：("spark", 3.2)。  
 # MAGIC 默认分区器：Hash分区规则。  
 # MAGIC 可以手动设置分区规则（rdd.partitionBy的方法来设置）。  
@@ -95,7 +143,19 @@ rdd4.collect()
 
 # MAGIC %md
 # MAGIC ### 特性5 RDD的分区规划，会尽量靠近数据所在的服务器
-# MAGIC 因为这样可以走`本地读取`，避免`网络读取`。  
+# MAGIC 
+# MAGIC - Optionally, a list of preferred locations to compute each split on (e.g. block locations for an HDFS file)
+# MAGIC 
+# MAGIC 在初始RDD（读取数据的时候）规划的时候，分区会尽量规划到存储数据所在的服务器上。
+# MAGIC 
+# MAGIC 因为这样可以走`本地读取`，避免`网络读取`。
+# MAGIC 
+# MAGIC 本地读取：Executor所在的服务器，同样是一个DataNode，同时这个DataNode上有他要读取的数据，所以可以直接读取机器硬盘即可，无需网络传输。
+# MAGIC 
+# MAGIC 网络读取：读取数据，需要经过网络的传输才能读取到。
+# MAGIC 
+# MAGIC 本地读取的性能远大于网络读取的性能。
+# MAGIC 
 # MAGIC > Spark会在`确保并行计算能力的前提下`，尽量确保本地读取。  
 # MAGIC > 这里是尽量确保，而不是100%确保。  
 # MAGIC > 所以这个特性也是可选的。
